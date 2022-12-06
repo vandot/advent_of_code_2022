12ee97d1ddd99abb3f98935ff90fb21a54ba8933
Ivan Vandot <ivan@vandot.rs>
2022-12-06 14:59:16 +0100

    solve 06 in one for loop

--- 06_tuning_trouble/tuning_trouble.pl
+++ 06_tuning_trouble/tuning_trouble.pl
@@ -1,4 +1,5 @@
 use strict; use warnings;
+no warnings 'uninitialized';
 use File::Basename;
 
 $\ = "\n";
@@ -6,25 +7,31 @@ $\ = "\n";
 my $filename = dirname(__FILE__) . "/LIST.txt";
 open(my $fh, "<", $filename) or die $!;
 my $chars = <$fh>;
+my $sop;
+my $som;
 
 for my $i (1 .. length $chars) {
-    my @char = split //, substr($chars, $i-1, 4);
-    my %uniq; 
-    @uniq{@char} = (); 
-    my $count = scalar keys %uniq;
-    if ($count == 4) {
-        print($i+3);
-        last;
+    if (length $sop == 0) {
+        my @char_sop = split //, substr($chars, $i-1, 4);
+        my %uniq_sop; 
+        @uniq_sop{@char_sop} = (); 
+        my $count_sop = scalar keys %uniq_sop;
+        if ($count_sop == 4) {
+            $sop = $i + 3;
+        }
     }
-}
-
-for my $i (1 .. length $chars) {
-    my @char = split //, substr($chars, $i-1, 14);
-    my %uniq; 
-    @uniq{@char} = (); 
-    my $count = scalar keys %uniq;
-    if ($count == 14) {
-        print($i+13);
+    if (length $som == 0) {
+        my @char_som = split //, substr($chars, $i-1, 14);
+        my %uniq_som; 
+        @uniq_som{@char_som} = (); 
+        my $count_som = scalar keys %uniq_som;
+        if ($count_som == 14) {
+            $som = $i + 13
+        }
+    }
+    if ((length $sop != 0) && (length $som != 0)) {
+        print($sop);
+        print($som);
         last;
     }
 }
